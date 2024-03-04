79e82a1828b3a31017d4c8db92c39527437fb032
hfdavis <149373126+hfdavis@users.noreply.github.com>
2024-03-04 10:02:53 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div classname="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Holly Davis and is open sourced on{" "}
+          <a
+            href="https://github.com/hfdavis/react-weather-app-final-project"
+            target="blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
