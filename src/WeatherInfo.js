84c088f53ace4b7ce4e1c6cd8cb2d97eabc062ef
hfdavis <149373126+hfdavis@users.noreply.github.com>
2024-03-06 16:23:23 +0000

    added unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon.js";
+import WeatherTemperature from "./WeatherTemperature.js";
 
 export default function WeatherInfo(props) {
   return (
@@ -18,10 +19,7 @@ export default function WeatherInfo(props) {
             <WeatherIcon code={props.data.icon} />
           </div>
 
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul className="conditions">
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function ShowCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature"> {Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a className="text-decoration-none" href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature"> {Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a className="text-decoration-none" href="/" onClick={ShowCelsius}>
+            °C |
+          </a>{" "}
+          °F
+        </span>
+      </div>
+    );
+  }
+}
